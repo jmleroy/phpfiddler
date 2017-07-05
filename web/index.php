a98b7a57056db76fda9cdbc44b0c068141e93be6
Jean-Marc Leroy <jmleroy@gmail.com>
2017-07-05 11:27:48 +0200

    adds a separate directory and route for user's fiddles

--- web/index.php
+++ web/index.php
@@ -6,9 +6,18 @@ $app = new Silex\Application();
 $app->get('/', function() {
     return 'Hello world';
 });
+
 $app->get('/examples/{name}', function($name) use ($app) {
     ob_start();
     include (__DIR__ . '/../examples/' . $name . '.php');
+    $body = ob_end_flush();
+
+    return $body;
+});
+
+$app->get('/fiddles/{name}', function($name) use ($app) {
+    ob_start();
+    include (__DIR__ . '/../fiddles/' . $name . '.php');
     $body = ob_end_flush();
 
     return $body;
