77c89262dc0acd7760f60e8b157170b0e2ee4264
Jean-Marc Leroy <jmleroy@gmail.com>
2017-07-06 09:26:42 +0200

    home page now displays formatted README.md

--- web/index.php
+++ web/index.php
@@ -1,11 +1,22 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use League\CommonMark\CommonMarkConverter;
+
 $app = new Silex\Application();
 $fiddle = new \PhpFiddler\Fiddle();
 
-$app->get('/', function() {
-    return 'Hello world';
+$app->get('/', function() use ($fiddle) {
+    ob_start();
+    $fiddle->start();
+    $converter = new CommonMarkConverter;
+    echo '<div class="readme">';
+    echo $converter->convertToHtml(file_get_contents(__DIR__ . '/../README.md'));
+    echo '</div>';
+    $fiddle->end();
+    $body = ob_get_clean();
+
+    return $body;
 });
 
 $app->get('/examples/{name}', function($name) use ($app, $fiddle) {
